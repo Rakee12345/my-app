33ca2c214a5f6d2c7528330bf86e5258581ce831
Rakee12345 <junna.rakesh@gmail.com>
2024-11-22 12:06:42 +0530

    student id search

--- src/app/student-id.service.ts
+++ src/app/student-id.service.ts
@@ -11,4 +11,8 @@ export class StudentIdService {
   getStudentId():Observable<any>{
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student");
   }
+
+  getFilteredStudentId(term: string): Observable<any> {
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student?filter="+term);
+  }
 }
