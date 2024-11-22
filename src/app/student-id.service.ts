190f3ac20339e6d4746bab7f694a950ea2593162
Rakee12345 <junna.rakesh@gmail.com>
2024-11-22 15:24:05 +0530

    studentid logo added

--- src/app/student-id.service.ts
+++ src/app/student-id.service.ts
@@ -24,6 +24,10 @@ export class StudentIdService {
   getPagedStudentId(limit:number , page: number):Observable<any>{
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student?limit="+limit+"&page="+page);
   }
+  createStudentId(data:any):Observable<any> {
+    return this._httpClient.post("https://6128991386a213001729f9df.mockapi.io/test/v1/student", data);
+
+  }
 
   deleteStudentId(studentId:string):Observable<any> {
     return this._httpClient.delete("https://6128991386a213001729f9df.mockapi.io/test/v1/student/"+studentId);
