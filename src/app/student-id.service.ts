9d9f5ccd5d0c0c2758975c1a04a0440d3eb8e090
Rakee12345 <junna.rakesh@gmail.com>
2024-11-22 14:48:53 +0530

    pagination

--- src/app/student-id.service.ts
+++ src/app/student-id.service.ts
@@ -14,5 +14,22 @@ export class StudentIdService {
 
   getFilteredStudentId(term: string): Observable<any> {
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student?filter="+term);
+
   }
+
+  getSortedStudentId(column:string, order: string):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student?sortBy="+column +"&order="+order);
+  }
+
+  getPagedStudentId(limit:number , page: number):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/student?limit="+limit+"&page="+page);
+  }
+
+  deleteStudentId(studentId:string):Observable<any> {
+    return this._httpClient.delete("https://6128991386a213001729f9df.mockapi.io/test/v1/student/"+studentId);
+
+  }
+
+
+
 }
